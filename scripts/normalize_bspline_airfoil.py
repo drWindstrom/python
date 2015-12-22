abfa17b251074a0573c81669fbe191fa8cd5167a
fred <fred@fred-ld.homeNET>
2015-12-22 21:39:18 +0100

    Added a view things

--- scripts/normalize_bspline_airfoil.py
+++ scripts/normalize_bspline_airfoil.py
@@ -7,12 +7,22 @@ from scipy import interpolate
 
 
 # Load airfoil from iges file
-iges_file = '../airfoils/airfoil_22000_rot.igs'
+iges_file = '../airfoils/airfoil_7000_rot.igs'
 tck = aft.load_airfoil_iges(iges_file)
 
 tck_norm = aft.norm_bspline_airfoil(tck)
 tck_norm_mod = aft.correct_te(tck_norm, s=0.0, k=3)
+num_points, points = aft.bspline_to_points(tck_norm_mod, min_step=1e-4,
+                                           max_step=0.01)
+print('num of points: {}'.format(num_points))
 
+fname = '/home/fred/cfd/grids/3d_reference_rotor/3d_ref_rot_r7000.dat'
+aft.write_pointwise_seg(points, fname)
+
+te_point = aft.find_te_point(tck_norm_mod)
+u_le, le_point = aft.find_le_point(tck_norm_mod, te_point)
+
+print('u location: {}'.format(u_le))
 
 # Plot airfoil
 plt.figure('Airfoil')
@@ -34,4 +44,12 @@ plt.plot(curvature_norm, '-bx', label='norm. airfoil')
 plt.plot(curvature_norm_mod, '-rx', label='norm. mod. airfoil')
 plt.grid()
 plt.legend()
+
+# Plot exported points
+plt.figure('exp. points')
+plt.plot(points[:, 0], points[:, 1], 'o', label='norm. airfoil')
+plt.axis('equal')
+plt.grid()
+plt.legend()
+
 plt.show()
